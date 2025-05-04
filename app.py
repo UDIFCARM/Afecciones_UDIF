fb134be09074777bb0a1c416e3d01587f9e2bba4
UDIFCARM <UDIFCARM@GMAIL.COM>
2025-05-04 09:42:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import uuid
 from datetime import datetime
 from docx import Document
 from branca.element import Template, MacroElement
+import pdfkit
 
 # Diccionario con los nombres de municipios y sus nombres base de archivo
 shp_urls = {
