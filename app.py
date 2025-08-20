5a7eb6926bedc671050af6acde9a38f5d98b3959
UDIFCARM <UDIFCARM@GMAIL.COM>
2025-08-20 17:03:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ def crear_mapa(lon, lat, afecciones=[], parcela_gdf=None):
 
     folium.raster_layers.WmsTileLayer(
         url="https://mapas-gis-inter.carm.es/geoserver/ows?SERVICE=WMS&?",
-        name="Catálogo de Montes de Utilidad Pública",
+        name="PFO_ZOR_DMVP_CARM:MONTES",
         fmt="image/png",
         layers="PFO_ZOR_DMVP_CARM:MONTES",
         transparent=True,
