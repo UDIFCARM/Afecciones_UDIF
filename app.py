fca0fc4f9d33a4d1b011bf406352f39e6d622e8b
UDIFCARM <UDIFCARM@GMAIL.COM>
2025-08-20 16:49:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ def crear_mapa(lon, lat, afecciones=[], parcela_gdf=None):
     ).add_to(m)
 
     folium.raster_layers.WmsTileLayer(
-        url="https://mapas-gis-inter.carm.es/geoserver/ows?service=WMS&version=1.3.0&request=GetLegendGraphic&format=image%2Fpng&width=20&height=20&layer=SIG_LUP_SITES_CARM%3ARN2000",
+        url="https://mapas-gis-inter.carm.es/geoserver/ows?SERVICE=WMS&?",
         name="SIG_LUP_SITES_CARM:RN2000",
         fmt="image/png",
         layers="SIG_LUP_SITES_CARM:RN2000",
