31791be6f0ac4d23ee1ff834aee0579c48908cdb
UDIFCARM <UDIFCARM@GMAIL.COM>
2025-10-18 10:46:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -502,13 +502,14 @@ def generar_pdf(datos, x, y, filename):
     pdf.set_text_color(255, 0, 0)
     pdf.set_draw_color(0, 0, 0) # Borde negro  
     pdf.set_line_width(0.5)
+    pdf.set_fill_color(200, 200, 200) # Fondo gris
     
-    # Parte 1: Texto en rojo y negrita
+    # Parte 1: Texto en rojo y negrita dentro de un cuadro con fondo gris
     pdf.set_text_color(255, 0, 0)  # Color rojo
     texto_rojo = (
         "Este borrador preliminar de afecciones no tiene el valor de una certificación oficial y por tanto carece de validez legal y solo sirve como información general con carácter orientativo."
     )
-    pdf.multi_cell(170, 8, texto_rojo, border=0, align="J")  # Sin borde individual
+    pdf.multi_cell(170, 8, texto_rojo, border=1, align="J", fill=True)  # Con borde, fondo gris y texto justificado
     pdf.ln(2)
 
     # Parte 2: Texto en negrita (sin rojo) para el resto del documento
