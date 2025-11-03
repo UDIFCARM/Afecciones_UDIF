0a296e7e868060ddccd409c8658135cd725b0ec5
UDIFCARM <UDIFCARM@GMAIL.COM>
2025-11-03 09:43:24 +0100

    Change font size in PDF generation

--- app.py
+++ app.py
@@ -520,7 +520,7 @@ def generar_pdf(datos, x, y, filename):
 
     # Nueva sección para el texto en cuadro
     pdf.ln(10)
-    pdf.set_font("Arial", "B", 8)
+    pdf.set_font("Arial", "B", 10)
     pdf.set_text_color(255, 0, 0)
     pdf.set_draw_color(0, 0, 0) # Borde negro  
     pdf.set_line_width(0.5)
@@ -567,7 +567,7 @@ def generar_pdf(datos, x, y, filename):
     pdf.ln(2)
 
     # Volver a negrita para el resto del texto
-    pdf.set_font("Arial", "B", 12)  # Restaurar negrita
+    pdf.set_font("Arial", "B", 10)  # Restaurar negrita
     texto_final = (
         "\nDe acuerdo con lo establecido en el artículo 22 de la ley 43/2003 de 21 de noviembre de Montes, toda inmatriculación o inscripción de exceso de cabida en el Registro de la Propiedad de un monte o de una finca colindante con monte demanial o ubicado en un término municipal en el que existan montes demaniales requerirá el previo informe favorable de los titulares de dichos montes y, para los montes catalogados, el del órgano forestal de la comunidad autónoma.\n\n"
         "En cuanto a vías pecuarias, salvaguardando lo que pudiera resultar de los futuros deslindes, en las parcelas objeto este informe-borrador, cualquier construcción, plantación, vallado, obras, instalaciones, etc., no deberían realizarse dentro del área delimitada como dominio público pecuario provisional para evitar invadir éste.\n\n"
